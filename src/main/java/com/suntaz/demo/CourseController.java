e98cbe6ac296884c81c785791bbdf1bddcb3e31f
Suntaz <Suntaz.Hl.Wu@ibm.com>
2022-10-27 12:07:07 +0800

    build api getAllCourses

---
+++ src/main/java/com/suntaz/demo/CourseController.java
@@ -0,0 +1,20 @@
+package com.suntaz.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	
+	@RequestMapping("courses")
+	public List<Course> getAllCourses(){
+		return Arrays.asList(
+			new Course(1,"java","Allen"),
+			new Course(2,"aws","Ben")
+		);
+	}
+
+}
